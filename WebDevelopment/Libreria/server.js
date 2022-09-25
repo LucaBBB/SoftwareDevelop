eaba447a2cedc2f1a86c3e81de56623dc9337a1d
LucaBBB <lucaborsalino98@gmail.com>
2022-09-25 16:58:52 +0200

    Ristrutturazione progetto

--- WebDevelopment/Libreria/server.js
+++ WebDevelopment/Libreria/server.js
@@ -3,49 +3,39 @@
 // import
 const express = require('express');
 const morgan = require('morgan');
-const sqlite = require('sqlite3');
+
+const dao = require('./dao.js');
 
 // init express
 const app = express();
 const port = 3000;
 
-const db = new sqlite.Database('./database.db', (err) => {
-    console.error(err);
-});
-
-
 app.use(morgan('tiny'));
 app.use(express.json());
 
-// routes
-app.get('/libri', (req, res) => {
-    const query = 'SELECT * from Libri';
-    db.all(query, (err, rows) => {
-        if (err)
-            throw err;
 
-        res.json(rows);
-    });
+app.get('/libri', (req, res) => {
+    dao.getTuttiLibri()
+        .then((libri) => res.json(libri))
+        .catch(() => res.status(500).end());
 });
 
 app.post('/add', (req, res) => {
-    const titolo = req.body.titolo;
-    const autore = req.body.autore;
-    const isbn = req.body.isbn;
     let completato = 0;
     if (req.body.completato) completato = 1;
 
-    const query = 'INSERT INTO Libri VALUES (?, ?, ?, ?)';
+    const nuovoLibro = {
+        titolo: req.body.titolo,
+        autore: req.body.autore,
+        isbn: req.body.isbn,
+        completato: completato
+    }
 
-    db.run(query, [titolo, autore, isbn, completato], (err) => {
-        if (err)
-            throw err;
-
-        res.end();
-    });
-
+    console.log(nuovoLibro);
+    dao.addNuovoLibro(nuovoLibro)
+        .then((result) => res.status(201).header('Location', `/libri/${result}`).end())
+        .catch((err) => res.status(503).json({ error: `Errore interno al database durante l'aggiunta`}));
 });
-
 
 
 app.listen(port, () =>
