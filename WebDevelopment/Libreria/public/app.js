508d7fe371241b0e0fcef1132077e05d83097293
LucaBBB <lucaborsalino98@gmail.com>
2022-10-08 22:49:58 +0200

    Modifiche di carattere generale

--- WebDevelopment/Libreria/public/app.js
+++ WebDevelopment/Libreria/public/app.js
@@ -68,17 +68,21 @@ class App {
     costruisciRiga(libro, index) {
         let tr = document.createElement('tr');
 
+
         let tdTitolo = document.createElement('td');
         tdTitolo.setAttribute("style", "vertical-align: middle;");
         tdTitolo.innerHTML = libro.titolo;
+
 
         let tdAutore = document.createElement('td');
         tdAutore.setAttribute("style", "vertical-align: middle;");
         tdAutore.innerHTML = libro.autore;
 
+
         let tdIsbn = document.createElement('td');
         tdIsbn.setAttribute("style", "vertical-align: middle;");
         tdIsbn.innerHTML = libro.isbn;
+
 
         let tdCompletato = document.createElement('td');
         tdCompletato.setAttribute("style", "vertical-align: middle;");
@@ -87,24 +91,51 @@ class App {
         checkbox.checked = libro.completato;
         tdCompletato.appendChild(checkbox);
 
-        let tdCancel = document.createElement('td');
-        var button = document.createElement("button");
-        button.setAttribute('class', 'btn btn-default cancelRow');
-        button.id = "cancel" + index;
-        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                            </svg>`;
-        button.addEventListener('click', () => {
+
+        let tdUpdate = document.createElement('td');
+        var buttonUpdate = document.createElement("button");
+        buttonUpdate.setAttribute('class', 'btn btn-outline-success cancelRow');
+        let iUpdate = document.createElement('i');
+        iUpdate.setAttribute('class', 'bi bi-pencil');
+        buttonUpdate.appendChild(iUpdate);
+        
+        buttonUpdate.addEventListener('click', () => {
             this.wrapperDeleteLibro(libro.isbn);
         });
-        tdCancel.appendChild(button);
+        tdUpdate.appendChild(buttonUpdate);
+
+
+        // buttonUpdate.setAttribute('type', 'button');
+        // buttonUpdate.setAttribute('data-toggle', 'tooltip');
+        // buttonUpdate.setAttribute('data-placement', 'top');
+        // buttonUpdate.setAttribute('title', 'Modifica libro');
+        
+
+        let tdCancel = document.createElement('td');
+        var buttonCancel = document.createElement("button");
+        buttonCancel.setAttribute('class', 'btn btn-outline-danger cancelRow');
+        let iCancel = document.createElement('i');
+        iCancel.setAttribute('class', 'bi bi-trash');
+        buttonCancel.appendChild(iCancel);
+        buttonCancel.addEventListener('click', () => {
+            this.wrapperDeleteLibro(libro.isbn);
+        });
+        tdCancel.appendChild(buttonCancel);
+        
+
+
+        // button.setAttribute('type', 'button');
+        // button.setAttribute('data-toggle', 'tooltip');
+        // button.setAttribute('data-placement', 'top');
+        // button.setAttribute('title', 'Elimina libro');
+       
 
 
         tr.appendChild(tdTitolo);
         tr.appendChild(tdAutore);
         tr.appendChild(tdIsbn);
         tr.appendChild(tdCompletato);
+        tr.appendChild(tdUpdate);
         tr.appendChild(tdCancel);
 
         return tr;
@@ -128,7 +159,7 @@ class App {
      * Metodo comune in più parti del codice, quindi estratto per evitare duplicazioni.
      * Effettua la chiamata al servizio che va a prendere i dati da db e a costruire la griglia.
      */
-     wrapperShowLibri() {
+    wrapperShowLibri() {
         this.bookManager.getLibri().then(libri => {
             this.tbody.empty();
             this.mostraLibri(libri);
