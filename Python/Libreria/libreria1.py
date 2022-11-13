7e6bf6ffb3d0a7f82896b3d3988dbe4ecc5175ba
LucaBBB <lucaborsalino98@gmail.com>
2022-11-13 11:37:05 +0100

    Aggiunta di un nuovo libro o incremento del numero di copie se gia' presente

--- Python/Libreria/libreria1.py
+++ Python/Libreria/libreria1.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Libro:
     def __init__(self, isbn, titolo, autore, anno, copie):
         self.isbn = isbn
@@ -23,6 +24,9 @@ filepath = "C:\\Users\\PC-di-Luca\\Desktop\\SoftwareDevelop\\Python\\Libreria\\d
 libreria = []
 
 
+# FUNZIONI DI INPUT/OUTPUT SUL DATASET
+
+# Funzione che legge dal dataset presente su un file JSON i libri che sono già posseduti e va ad inserirli nella lista "libreria" running correntemente.
 def inizializza_libreria():
     f = open(filepath)
     data = json.load(f)
@@ -33,16 +37,38 @@ def inizializza_libreria():
         autore = libro['autore']
         anno = libro['anno']
         copie = libro['copie']
-
-        json_to_libro = Libro(isbn, titolo, autore, anno, copie)
-
-        print(f'Letto da JSON il libro: {json_to_libro}')
-   
+        libreria.append(Libro(isbn, titolo, autore, anno, copie))
 
 
+def get_indice_libro_by_isbn(isbn):
+    for i in range(0, len(libreria)):
+        if libreria[i].isbn == isbn:
+            return i
+    return -1
 
 
+def aggiungi_libro():
+    print("--- Aggiunta di un libro ---")
 
+    isbn = input("Inserire l'isbn: ")
+    titolo = input("Inserire il titolo: ")
+    autore = input("Inserire l'autore: ")
+    anno = int(input("Inserire l'anno: "))
+    copie = int(input("Inserire il numero di copie: "))
+
+    indice_libro = get_indice_libro_by_isbn(isbn)
+
+    if indice_libro == -1:
+        libreria.append(Libro(isbn, titolo, autore, anno, copie))
+        print("Il nuovo libro è stato aggiunto nella libreria.")
+    else:
+        libreria[indice_libro].copie += copie
+        print(f'{copie} copie del libro già posseduto sono state aggiunte, nuovo info del libro: {libreria[indice_libro]}')
+
+    print("----------------------------")
+    
+
+# Funzione che stampa le informazioni di tutti i libri (se posseduti), altrimenti un messaggio di errore.
 def visualizza_libri():
     print("--- Visualizzazione di tutti i libri posseduti ---")
 
@@ -58,10 +84,17 @@ def visualizza_libri():
 
 
 def main():
-    # libro_test = Libro("0001", "Il visconte dimezzato", "Italo Calvino", 1951, 1)
-    # print(libro_test)
-    #visualizza_libri()
     inizializza_libreria()
+
+    scelta_utente = int(input("[0] per terminare, [1] per visualizzare i libri posseduti, [2] per aggiungere un nuovo libro: "))
+    while scelta_utente != 0:
+        if scelta_utente == 1:
+            visualizza_libri()
+        elif scelta_utente == 2:
+            aggiungi_libro()
+        scelta_utente = int(input("[0] per terminare, [1] per visualizzare i libri posseduti, [2] per aggiungere un nuovo libro: "))
+    if scelta_utente == 0:
+        print("Arrivederci, alla prossima!")
 
 
 print("")
