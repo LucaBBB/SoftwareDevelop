d8cccdd1840c5388bf11a2cebb403d85049030bf
LucaBBB <lucaborsalino98@gmail.com>
2022-11-14 21:39:06 +0100

    Rimozione libro, ricerca per isbn, scrittura su file

--- Python/Libreria/libreria1.py
+++ Python/Libreria/libreria1.py
@@ -14,17 +14,18 @@ class Libro:
 
 
 # TODO List:
-# 1) Visualizzare tutti i libri;
-# 2) Aggiungere un libro o n copie del libro esistente;
-# 3) Rimuovere un libro o n copie del libro esistente;
-# 4) Ricerca per un qualche criterio.
+# 1) Visualizzare tutti i libri;                                            OK
+# 2) Aggiungere un libro o n copie del libro esistente;                     OK
+# 3) Rimuovere un libro o n copie del libro esistente;                      OK
+# 4) Ricerca per un qualche criterio.                                       ISBN(OK)
 
 # Zona variabili
-filepath = ".\\dataset.json"
+filepath = "C:\\Users\\PC-di-Luca\\Desktop\\SoftwareDevelop\\Python\\Libreria\\dataset.json"
 libreria = []
 
-
+# ------------------------------------
 # FUNZIONI DI INPUT/OUTPUT SUL DATASET
+# ------------------------------------
 
 # Funzione che legge dal dataset presente su un file JSON i libri che sono già posseduti e va ad inserirli nella lista "libreria" running correntemente.
 def inizializza_libreria():
@@ -39,12 +40,18 @@ def inizializza_libreria():
         copie = libro['copie']
         libreria.append(Libro(isbn, titolo, autore, anno, copie))
 
+# Funzione che scrive sul dataset al termine dell'esecuzione del programma.
+def salva_libreria():
+    with open(filepath, "w") as file:
+        json.dump([ob.__dict__ for ob in libreria], file)
 
+# Funzione di supporto che estrapola la posizione nella lista del libro con l'isbn coincidente con quello passato come parametro, -1 se non trovato.
 def get_indice_libro_by_isbn(isbn):
     for i in range(0, len(libreria)):
         if libreria[i].isbn == isbn:
             return i
     return -1
+# ------------------------------------
 
 
 def aggiungi_libro():
@@ -81,10 +88,20 @@ def rimuovi_libro():
     else:
         if libreria[indice_libro].copie == 1 or (libreria[indice_libro].copie - copie <= 0):
             libreria.pop(indice_libro)
+            print(f'Libro con isbn {isbn} è stato rimosso.')
         else:
             libreria[indice_libro].copie -= copie
+            print(f'{copie} copie del libro già posseduto sono state rimosse, nuovo info del libro: {libreria[indice_libro]}')
 
     print("----------------------------")
+
+def info_libro_isbn():
+    isbn = print("Inserire l'isbn del libro da cercare: ")
+    index = get_indice_libro_by_isbn(isbn)
+    if index == -1:
+        print(f'Nessun libro con isbn {isbn} posseduto.')
+    else:
+        print(f'Trovato libro: {libreria[index]}')
 
 # Funzione che stampa le informazioni di tutti i libri (se posseduti), altrimenti un messaggio di errore.
 def visualizza_libri():
@@ -101,10 +118,15 @@ def visualizza_libri():
     print("--------------------------------------------------")
 
 
+def menu():
+    print("[0] per terminare, [1] per visualizzare i libri posseduti, [2] per aggiungere un nuovo libro, [3] per rimuovere un libro, [4] per cercare un libro per ISBN: ")
+
 def main():
     inizializza_libreria()
 
-    scelta_utente = int(input("[0] per terminare, [1] per visualizzare i libri posseduti, [2] per aggiungere un nuovo libro: "))
+    menu()
+    scelta_utente = int(input())
+
     while scelta_utente != 0:
         if scelta_utente == 1:
             visualizza_libri()
@@ -112,9 +134,13 @@ def main():
             aggiungi_libro()
         elif scelta_utente == 3:
             rimuovi_libro()
-        scelta_utente = int(input("[0] per terminare, [1] per visualizzare i libri posseduti, [2] per aggiungere un nuovo libro: "))
+            
+        menu()
+        scelta_utente = int(input())
+
     if scelta_utente == 0:
         print("Arrivederci, alla prossima!")
+        salva_libreria()
 
 
 print("")
