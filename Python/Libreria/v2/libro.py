d7642318d65ea0b15ddc141719b4008cc4c3d04b
LucaBBB <lucaborsalino98@gmail.com>
2022-11-20 22:26:42 +0100

    Setup

--- Python/Libreria/v2/libro.py
+++ Python/Libreria/v2/libro.py
@@ -1,11 +1,12 @@
 class Libro:
 
-    def __init__(self, isbn, titolo, autore, anno, copie):
+    def __init__(self, isbn, titolo, autore, editore, anno, copie):
         self.isbn = isbn
         self.titolo = titolo
         self.autore = autore
+        self.editore = editore
         self.anno = anno
         self.copie = copie
 
     def __str__(self):
-        return f'{self.titolo}, {self.autore}, {self.anno}, {self.isbn}, {self.copie}'
+        return f'{self.titolo}, {self.autore}, {self.editore}, {self.anno}, {self.isbn}, {self.copie}'
